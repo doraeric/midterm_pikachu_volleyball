92925250546e496d6132daeb766b061b2f0bebaa
doraeric <benson.doraemon@gmail.com>
2019-05-21 16:38:34 +0800

    Updates

---
+++ precache-manifest.3e38505dcde5d5d73d10bb2835e02b5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4ef4b43e84f27d6c33f4889d8fa5ad",
+    "url": "/midterm_pikachu_volleyball/index.html"
+  },
+  {
+    "revision": "0881f87250435cc84b18",
+    "url": "/midterm_pikachu_volleyball/static/css/main.8b989782.chunk.css"
+  },
+  {
+    "revision": "907d01ff84709033816a",
+    "url": "/midterm_pikachu_volleyball/static/js/2.4996c8e5.chunk.js"
+  },
+  {
+    "revision": "0881f87250435cc84b18",
+    "url": "/midterm_pikachu_volleyball/static/js/main.b4e0f955.chunk.js"
+  },
+  {
+    "revision": "4dfee631a76d7af022bd",
+    "url": "/midterm_pikachu_volleyball/static/js/runtime~main.7cfa1605.js"
+  }
+]);
